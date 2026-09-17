b6de48002165b44c8cd2889f8310074fba17b8bf
jjnhwg <145168531+jjnhwg@users.noreply.github.com>
2026-09-17 18:28:02 -0400

    Add: is-palindrome - submission-35

---
+++ Data Structures & Algorithms/is-palindrome/submission-35.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        '''
+        '''
+
+        l = 0 
+        r = len(s) - 1 
+
+        while l < r:
+            while l < r:
+                if s[l].isalnum():
+                    break
+                l += 1 
+            
+            while l < r: 
+                if s[r].isalnum():
+                    break 
+                r -= 1 
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1 
+            r -= 1 
+        
+
+        return True
+        
+
+            
